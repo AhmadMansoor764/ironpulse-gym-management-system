29315dd461de3c731419af9be84f6ddee40e9fc4
AhmadMansoor764 <am.web.dev2024@gmail.com>
2026-08-19 12:48:08 +0430

    Prepare frontend for production

--- Backend/controller/authcontroller.js
+++ Backend/controller/authcontroller.js
@@ -99,9 +99,11 @@ export const login = async (req, res) => {
     });
 
     res.cookie("token", token, {
-      httpOnly: true,
-      expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
-    });
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+  maxAge: 24 * 60 * 60 * 1000,
+});
 
     res.status(200).json({
       success: true,
